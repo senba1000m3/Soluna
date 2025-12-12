7c6d9b3e85f7319439beca3bc6f98008d965e2b6
senba <senba1000m3@gmail.com>
2025-12-12 20:31:21 +0800

    Chores: Repair solaris Dockerfile

---
+++ Solaris/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Soluna } from "./pages/Soluna";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Soluna />
+  </React.StrictMode>,
+);
